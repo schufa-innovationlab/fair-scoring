a6373bc195cd819e960af490a52cc3610747cfc7
Klaus Broelemann <klaus.broelemann@schufa.de>
2024-06-16 23:58:43 +0200

    X-Axis: use original score range

--- src/fairscoring/plots/_calibration.py
+++ src/fairscoring/plots/_calibration.py
@@ -22,6 +22,7 @@ def plot_groupwise_score_calibration(
         strategy: str = 'uniform',
         rescale: bool = True,
         rescale_by: Optional[List] = None,
+        prefer_high_scores: bool = True,
         figsize: tuple = (10, 5)):
     """
 
@@ -61,6 +62,9 @@ def plot_groupwise_score_calibration(
     rescale
         True if the values are rescaled
 
+    prefer_high_scores: bool, optional
+            Specify whether high scores or low scores are favorable.
+
     figsize: tuple
         Size of the figure.
 
@@ -80,9 +84,21 @@ def plot_groupwise_score_calibration(
     else:
         rescale_by = [0, 1]
 
+    # If smaller scores are better, then swap order when rescaling
+    if not prefer_high_scores:
+        rescale_by = [rescale_by[1], rescale_by[0]]
+
+    # Rescale scores
+    scores = (scores - rescale_by[0]) / (rescale_by[1] - rescale_by[0])
+
+    # Helper Function project scaled values back to the original Space
+    def inverse_scaling(scores):
+        return np.asarray(scores) * (rescale_by[1] - rescale_by[0]) + rescale_by[0]
+
     # Create figure &
     plt.figure(figsize=figsize)
-    plt.plot([1, 0], [0, 1], c='black')
+    x_limits = inverse_scaling([1, 0])
+    plt.plot(x_limits, [0, 1], c='black')
 
     # Iterate Groups
     for i, grp_flt in enumerate(split_groups(attribute, groups)):
@@ -102,14 +118,19 @@ def plot_groupwise_score_calibration(
                 # Bootstrap Sampling
                 idx_boot = np.random.choice(idx, size=len(idx), replace=True)
 
-                # Compute & Plot Bootstrap Calibration Curve
-                prob_true, prob_pred = calibration_curve(target[idx_boot], (scores[idx_boot] - rescale_by[0]) / (rescale_by[1] - rescale_by[0]),
-                                                         n_bins=n_bins, strategy=strategy)
-                plt.plot(prob_pred, prob_true, c=color_light, linewidth=1)
+                # Compute Bootstrap Calibration Curve
+                prob_true, prob_pred = calibration_curve(target[idx_boot], scores[idx_boot], n_bins=n_bins, strategy=strategy)
 
-        # Compute & Plot Bootstrap Calibration Curve
-        prob_true, prob_pred = calibration_curve(target[idx], (scores[idx] - rescale_by[0]) / (rescale_by[1] - rescale_by[0]),
-                                                 n_bins=n_bins, strategy=strategy)
+                # Undo scaling for original scores on x-axis
+                prob_pred = inverse_scaling(prob_pred)
+
+                plt.plot(prob_pred, prob_true, c=color, alpha=0.5, linewidth=1)
+
+        # Compute Calibration Curve
+        prob_true, prob_pred = calibration_curve(target[idx], scores[idx], n_bins=n_bins, strategy=strategy)
+
+        # Undo scaling for original scores on x-axis
+        prob_pred = inverse_scaling(prob_pred)
         plt.plot(prob_pred, prob_true, c=color, marker='o', linewidth=1)
 
     # Label Plots
