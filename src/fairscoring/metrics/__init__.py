36f6b0a02358f52da81df09143dd1b31b12b0f60
Klaus Broelemann <klaus.broelemann@schufa.de>
2024-08-07 13:38:02 +0200

    Independence is no default anymore

--- src/fairscoring/metrics/__init__.py
+++ src/fairscoring/metrics/__init__.py
@@ -4,13 +4,12 @@ A collection fairness metrics to measure the bias of scores.
 The package consists of multiple classes that cover different fairness concepts and distance metrics.
 A concrete fairness metric is an instance of these classes with a concrete parametrization.
 
-There are recommended default instances are already included:
+There are recommended **default instances** are already included:
 
 ==================  =========================
 Metric              Bias
 ==================  =========================
 :const:`bias_cal`   Calibration Bias
-:const:`bias_ind`   Independence Bias
 :const:`bias_eo`    Equal Opportunity Bias
 :const:`bias_pe`    Predictive Equality Bias
 ==================  =========================
@@ -19,9 +18,8 @@ Metric              Bias
 from .calibration import CalibrationMetric
 from .integral import WassersteinMetric
 
-__all__ = ['bias_cal', 'bias_ind', 'bias_eo', 'bias_pe']
+__all__ = ['bias_cal', 'bias_eo', 'bias_pe']
 
 bias_cal = CalibrationMetric(name="Calibration", score_transform="quantile")
-bias_ind = WassersteinMetric(fairness_type="IND", name="Independence", score_transform="quantile")
 bias_eo = WassersteinMetric(fairness_type="EO", name="Equal Opportunity", score_transform="quantile")
 bias_pe = WassersteinMetric(fairness_type="PE", name="Predictive Equality", score_transform="quantile")
